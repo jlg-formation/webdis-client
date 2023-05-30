df6182f508a1ba57ddc526a6eddee2de3323e309
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-05-30 20:29:26 +0200

    dev: command

--- front/src/webdis/Webdis.ts
+++ front/src/webdis/Webdis.ts
@@ -1,15 +1,14 @@
 class Webdis {
   tested = false
   url = 'http://127.0.0.1:7379'
-  async send(command: string): Promise<any> {
-    console.log('command: ', command)
-    const response = await fetch(this.url + '/' + this.parseCommand(command))
+  async send(...args: string[]): Promise<any> {
+    const response = await fetch(this.url + '/' + this.parseCommand(...args))
     const json: object = await response.json()
     console.log('json: ', json)
     return json
   }
-  parseCommand(command: string) {
-    return command.replace(/ /g, '/')
+  parseCommand(...args: string[]) {
+    return args.join('/')
   }
 }
 
