acc5a9320c6c62fe28f6210122d4cb51ce4ad4d6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-05-30 19:39:39 +0200

    dev: added hyperloglog dom and css

--- front/src/views/HyperLogLogView.vue
+++ front/src/views/HyperLogLogView.vue
@@ -1,15 +1,51 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const cardinality = ref(0)
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <main>
+    <h1>HyperLogLog</h1>
+    <div class="content">
+      <form class="add">
+        <label>
+          <span>Nbr of random elements to add in the set</span>
+          <input type="number" value="1" />
+        </label>
+        <button>Add</button>
+      </form>
+      <form class="get">
+        <button>Get cardinality</button>
+        <div class="cardinality">
+          {{ cardinality }}
+        </div>
+      </form>
+    </div>
+  </main>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
+<style scoped lang="scss">
+div.content {
+  display: flex;
+  flex-flow: column;
+  align-items: start;
+  gap: 0.5em;
+}
+form.add {
+  display: flex;
+  align-items: end;
+  gap: 0.5em;
+}
+
+form.get {
+  display: flex;
+  gap: 0.5em;
+
+  div.cardinality {
     display: flex;
     align-items: center;
+    justify-content: center;
   }
 }
 </style>
