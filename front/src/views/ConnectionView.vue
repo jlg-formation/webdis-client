b8d295a6e881690dbd6b8e086dbb461ece81301c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-05-31 11:25:24 +0200

    dev: title and favicon

--- front/src/views/ConnectionView.vue
+++ front/src/views/ConnectionView.vue
@@ -6,6 +6,14 @@ import PingPong from '@/components/redis/PingPong.vue'
   <main>
     <h1>Setup a Webdis connection</h1>
     <PingPong />
+    <div class="description">
+      <p>See the webdis project to start a redis on your machine.</p>
+      <p>
+        <a href="https://github.com/nicolasff/webdis" target="_blank"
+          >https://github.com/nicolasff/webdis</a
+        >
+      </p>
+    </div>
   </main>
 </template>
 
