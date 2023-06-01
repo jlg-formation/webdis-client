9429e5efb963686cc6cb08ff9707e8502c0db195
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-06-01 16:04:02 +0200

    dev: vite config

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -9,7 +9,10 @@ export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    outDir: '../docs',
+  },
 })
