f7be6a78eb892c9dd09180278db73fc216aa8adc
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-06-02 11:34:16 +0200

    dev: use hashbang
    because github does not support URL Rewriting for SPA

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,10 +1,11 @@
 import { useWebdisStore } from '@/stores/webdis'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import ConnectionView from '../views/ConnectionView.vue'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+
   routes: [
     {
       path: '/',
